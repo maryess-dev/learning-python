1bc3653de7683d6ca83dd69994f2d14a95d83fe0
Mariya Marnopolskaya <a1@MacBook-Pro-1.local>
2026-08-08 13:34:09 +0300

    added 2 tasks of 3 lesson

---
+++ lesson-3/1.py
@@ -0,0 +1,6 @@
+animal_type = input("Введите вид питомца: ")
+animal_age = input("Введите возраст питомца: ")
+animal_name = input("Введите кличку питомца: ")
+
+print(f'Это {animal_type} по кличке "{animal_name}". Возраст: {animal_age} года.')
+
