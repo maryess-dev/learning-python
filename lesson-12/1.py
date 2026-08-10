1a636f4efb94db738c039f0d7c330a6a06891ae5
Mariya Marnopolskaya <a1@MacBook-Pro-1.local>
2026-08-10 19:32:57 +0300

    added task of 12 lesson

---
+++ lesson-12/1.py
@@ -0,0 +1,25 @@
+import random
+
+matrix_1 = []
+matrix_2 = []
+matrix_3=[]
+rows = 10
+cols = 10
+
+for i in range(rows):
+  row_1 = []
+  row_2 = []
+  for j in range(10):
+    row_1.append(random.randint(-100, 100))
+    row_2.append(random.randint(-100, 100))
+  matrix_1.append(row_1)
+  matrix_2.append(row_2)
+
+for i in range(rows):
+  sum = []
+  for j in range(cols):
+    sum.append(matrix_1[i][j]+matrix_2[i][j])
+  matrix_3.append(sum)
+
+for row in matrix_3:
+  print(row)
